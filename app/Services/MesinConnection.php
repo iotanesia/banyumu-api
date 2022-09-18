ff173ae692d3c893e01f62bc7095a15792af769b
rexy siwabessy <rexysiwabessy@gmail.com>
2022-09-18 11:48:28 +0700

    update

--- app/Services/MesinConnection.php
+++ app/Services/MesinConnection.php
@@ -12,8 +12,8 @@ class MesinConnection {
             ->withoutVerifying()
             ->contentType("application/json")
             ->get('https://api.thingspeak.com/update?api_key=Q4QD7S1ARAHYELT8&amp;amp;field2=1');
-            Log::info(json_encode($response->json()));
-            if($response == 0) {
+            Log::info($response);
+            if($response == '0') {
                 self::turnOn('Q4QD7S1ARAHYELT8');
             } else {
                 return $response;
@@ -29,8 +29,8 @@ class MesinConnection {
             ->withoutVerifying()
             ->contentType("application/json")
             ->get('https://api.thingspeak.com/update?api_key=Q4QD7S1ARAHYELT8&amp;amp;field1='.$debit);
-            Log::info(json_encode($response->json()));
-            if($response == 0) {
+            Log::info($response);
+            if($response == '0') {
                 self::turnOn('Q4QD7S1ARAHYELT8');
             } else {
                 return $response;
