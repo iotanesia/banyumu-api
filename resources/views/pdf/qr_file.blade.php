c547d8d697695a4c4efa261f0671f06c46657637
rexy siwabessy <rexysiwabessy@gmail.com>
2023-02-24 13:38:11 +0700

    update

--- resources/views/pdf/qr_file.blade.php
+++ resources/views/pdf/qr_file.blade.php
@@ -49,7 +49,7 @@
 <body>
     <table>
         @foreach($data as $val)
-        <img class="img-qr" src="{{url('/')}}/qrcode_trx_file/{{$val['code']}}" alt="">
+        <img class="img-qr" src="{{url('/')}}/qrcode_trx_file/{{$val['code']}}.png" alt="">
         <span>{{$val['code']}}</span>
         <span>{{url('/')}}/qrcode_trx_file/{{$val['code']}}</span>
         @endforeach
